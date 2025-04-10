2314786c23698bc32412bda6240ff6935347d4ab
Erertertet <2589606498@gmail.com>
2025-04-10 23:49:14 +0100

    working script for image prompting

--- sample_for_image_prompting.py
+++ sample_for_image_prompting.py
@@ -1,4 +1,4 @@
-# https://huggingface.co/docs/diffusers/main/en/api/pipelines/stable_diffusion/stable_diffusion_3
+# https://huggingface.co/docs/diffusers/v0.33.1/api/pipelines/stable_diffusion/stable_diffusion_3
 
 import torch
 from PIL import Image
@@ -16,7 +16,7 @@ feature_extractor = SiglipImageProcessor.from_pretrained(
 image_encoder = SiglipVisionModel.from_pretrained(
     image_encoder_id,
     torch_dtype=torch.float16
-).to( "cuda")
+).to("cuda")
 
 pipe = StableDiffusion3Pipeline.from_pretrained(
     "stabilityai/stable-diffusion-3.5-large",
@@ -25,19 +25,18 @@ pipe = StableDiffusion3Pipeline.from_pretrained(
     image_encoder=image_encoder,
 ).to("cuda")
 
-pipe.load_ip_adapter(ip_adapter_id)
-pipe.set_ip_adapter_scale(0.6)
+pipe.load_ip_adapter(ip_adapter_id, weight_name="ip-adapter.bin")
+pipe.set_ip_adapter_scale(0.1)
 
-ref_img = Image.open("image.jpg").convert('RGB')
+ref_img = Image.open("img/image.png").convert('RGB')
 
 image = pipe(
     width=1024,
     height=1024,
-    prompt="a cat",
-    negative_prompt="lowres, low quality, worst quality",
+    prompt="minecraft cobble stone texture, smooth the pattern",
     num_inference_steps=24,
-    guidance_scale=5.0,
+    guidance_scale=0.8,
     ip_adapter_image=ref_img
 ).images[0]
 
-image.save("result.jpg")
+image.save("img/result.png")
